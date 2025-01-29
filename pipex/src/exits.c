efa23703c8c86621a3f58952faf5d8edcf59dc44
VICXPTRV <viktoriavorontsova.main@gmail.com>
2025-01-29 17:23:17 +0700

    OK

--- pipex/src/exits.c
+++ pipex/src/exits.c
@@ -6,7 +6,7 @@
 /*   By: vvoronts <vvoronts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 18:02:23 by vvoronts          #+#    #+#             */
-/*   Updated: 2025/01/29 17:08:07 by vvoronts         ###   ########.fr       */
+/*   Updated: 2025/01/29 17:21:10 by vvoronts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,6 @@ int	wait_processes(t_context *p)
 		else if (pid == p->in->pid)
 			perror_message(WEXITSTATUS(status));
 	}
-	// if (exitcode == EXEC || exitcode == NO_CMD)
-	// 	exitcode = EXECUTE;
-	// else if (exitcode != SUCCESS && exitcode != EXECUTE)
-	// 	exitcode = STERROR;
 	return (exitcode);
 }
 
@@ -55,13 +51,6 @@ void	perror_message(int err)
 
 void	error_exit(int err, t_context *p)
 {
-	// int	exitcode;
-
-	// perror_message(err);
-	// if (err == EXEC || err == NO_CMD)
-	// 	exitcode = EXECUTE;
-	// else
-	// 	exitcode = STERROR;
 	close_pipe(p);
 	cleanup(p);
 	exit(err);
@@ -72,31 +61,22 @@ void	validate_args(int argc, char **argv)
 	if (argc != 5)
 	{
 		perror_message(ARGS);
-		// error_exit(ARGS, NULL);
-	}
-	if (!argv[1][0] && !argv[4][0])
-	{
-		perror_message(NO_FILE);
-		perror_message(NO_FILE);
 		exit(STERROR);
-		// error_exit(NO_FILE, NULL);
 	}
 	if (!argv[1][0] || !argv[4][0])
 	{
-		error_exit(NO_FILE, NULL);
+		if (!argv[1][0])
+			perror_message(NO_FILE);
+		if (!argv[4][0])
+			perror_message(NO_FILE);
 		exit(STERROR);
-	}
-	if (!argv[2][0] && !argv[3][0])
-	{
-		perror_message(NO_CMD);
-		perror_message(NO_CMD);
-		exit(STERROR);
-		// error_exit(NO_CMD, NULL);
 	}
 	if (!argv[2][0] || !argv[3][0])
 	{
-		perror_message(NO_CMD);
+		if (!argv[2][0])
+			perror_message(NO_CMD);
+		if (!argv[3][0])
+			perror_message(NO_CMD);
 		exit(STERROR);
 	}
-		// error_exit(NO_CMD, NULL);
 }
--- pipex/src/main.c
+++ pipex/src/main.c
@@ -6,7 +6,7 @@
 /*   By: vvoronts <vvoronts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 10:59:11 by vvoronts          #+#    #+#             */
-/*   Updated: 2025/01/28 13:26:23 by vvoronts         ###   ########.fr       */
+/*   Updated: 2025/01/29 17:17:55 by vvoronts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	main(int argc, char **argv, char **envp)
 {
-	int			exitcode;
 	t_context	*p;
+	int			exitcode;
 
 	exitcode = 0;
 	validate_args(argc, argv);
