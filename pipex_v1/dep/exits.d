444c45faa92667255bb43a28dd308ce77d223b5c
Viktoriia Vorontsova <vvoronts@z1t1c1.42bangkok.com>
2025-02-08 09:47:27 +0700

    clean version, norm ok, no leaks, fds ok

--- pipex_v1/dep/exits.d
+++
@@ -1,2 +0,0 @@
-obj/exits.o: src/exits.c include/pipex.h lib/include/libft.h \
- lib/include/ft_printf.h lib/include/libft.h lib/include/elibft.h
--- pipex_v1/dep/getenv.d
+++
@@ -1,2 +0,0 @@
-obj/getenv.o: src/getenv.c include/pipex.h lib/include/libft.h \
- lib/include/ft_printf.h lib/include/libft.h lib/include/elibft.h
